7a4f0df26a5583183e969500552e96c51b7b84cc
Ruksana-Kauser <95047090+Ruksana-Kauser@users.noreply.github.com>
2022-03-11 01:07:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import pickle as pkl
+import numpy as np
+import sklearn as sk
+
+model = pkl.load(open("final_model.pkl","rb"))
+
+st.title("Farmer's Helper")
+st.write("This app helps farmer to predict the crop for cultivatoion based on climatic conditions and the mineral content of the soil.")
+st.write("Please choose the respective values from below  sliders:")
+
+x1 = st.slider('Temperature of the field', 8.82, 43.67)
+x2 = st.slider('Humidity of the field', 18.09, 99.98)
+x3 = st.slider('PH value of the field',4.50, 7.99)	
+x4 = st.slider('Rain Fall of the field', 40.35, 298.56)
+x5 = st.slider('Nitrate of the field', 20.00, 120.0)	
+x6 = st.slider('phropus of the field',10.0, 125.07)
+x7 = st.slider('potashm of the field', 10.0, 200.00)
+
+x = np.array([[x1,x2,x3,x4,x5,x6,x7]])
+ans = model.predict(x)
+st.text(ans)
